c7d226610fa4b0461c59d722ee96bc79dce41693
Karjeje <193631637+Karjeje@users.noreply.github.com>
2025-09-10 12:00:55 +0200

    Add code for clicking operators, equal sign, tweak
    Wrote the code that causes the first value that is entered to be saved from the variable a into the varible b, when one of the operator buttons are clicked. Then the second value is saved into the variable a. The display is also cleared, when clicking one of the operators. Also referenced the equal sign and made it so that clicking it logs the result in the console. The operation depends on the operator clicked. Still have to add code to display result in the display.
    
    Also in the function operate I had to add return before the call of each function, because undefined was logged in the console instead of the result.

--- calculator.js
+++ calculator.js
@@ -2,46 +2,72 @@ function add(a, b) {
         return a + b;
       }
 
-      function subtract(a, b) {
+function subtract(a, b) {
         return a - b;
       }
 
-      function multiply(a, b) {
+function multiply(a, b) {
         return a * b;
       }
 
-      function divide(a, b) {
+function divide(a, b) {
         return a / b;
       }
 
-      let a
-      let b
-      let operator
+let a
+let b
+let operator
 
-      function operate(a, b, operator) {
-        if (operator === "plus") {
-            add(a, b);
-        }
-        else if (operator === "minus") {
-            subtract(a,b);
-        }
-        else if (operator === "multiplication") {
-            multiply(a, b)
-        }
-        else if (operator === "division") {
-            divide(a, b)
-        }
-      }
+function operate(a, b) {
+    if (operator === "+") {
+        return add(a, b);
+    }
+    else if (operator === "-") {
+        return subtract(a,b);
+    }
+    else if (operator === "x") {
+        return multiply(a, b)
+    }
+    else if (operator === "÷") {
+        return divide(a, b)
+    }
+}
 
-      const display = document.querySelector(".display")
-      const digits = document.querySelectorAll(".digit")
+const display = document.querySelector(".display");
+const digits = document.querySelectorAll(".digit");
 
-      const MAX_LENGTH = 12;
+a = "";
+b= "";
 
-      digits.forEach((button) => {
-        button.addEventListener("click", function() {
-            if (display.textContent.length < MAX_LENGTH) {
-              display.textContent += button.textContent;
-            }
-        })
-      })
+digits.forEach((button) => {
+    button.addEventListener("click", function() {
+        if (display.textContent.length < 12) {
+            display.textContent += button.textContent;
+            a = display.textContent;
+            console.log("a:", a);
+        }
+    })
+})
+
+const operators = document.querySelectorAll(".operator");
+operator = "";
+
+operators.forEach((button) => {
+    button.addEventListener("click", function() {
+        operator = button.textContent;
+        b = a;
+        a = "";
+        display.textContent = "";
+        console.log("b:", b);
+    })
+})
+
+const equals = document.querySelector(".equals");
+
+equals.addEventListener("click", function() {
+    x = Number(a);
+    y = Number(b);
+    let result = operate(x,y);
+    console.log(result);
+})
+
